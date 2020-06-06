bdbc4078baab8d6c81b36dc4c1bac56478865fd0
alexander857 <00355519@uca.edu.sv>
2020-06-06 14:34:15 -0600

    Add: class Blocks

---
+++ Arkanoid/Blocks.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Arkanoid
+{
+    public class Blocks : PictureBox
+    {
+        public int hits { get; set; }
+        public bool destroyed { get; set; }
+
+        public Blocks() : base(){}
+    }
+}
