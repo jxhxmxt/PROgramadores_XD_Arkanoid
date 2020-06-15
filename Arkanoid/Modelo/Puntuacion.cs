99b4a57ad6e200d3e180686bd19175558799a340
jxhxmxt <62307163+jxhxmxt@users.noreply.github.com>
2020-06-15 13:05:11 -0600

    Adding Models classes

---
+++ Arkanoid/Modelo/Puntuacion.cs
@@ -0,0 +1,18 @@
+﻿namespace Arkanoid.Modelo
+{
+    public class Puntuacion
+    {
+        public int Id_p { get; set; }
+        public int Id_u { get; set; }
+        public int Puntaje { get; set; }
+        
+        public Puntuacion() { }
+
+        public Puntuacion(int pId_p, int pId_u, int pPuntaje)
+        {
+            Id_p = pId_p;
+            Id_u = pId_u;
+            Puntaje = pPuntaje;
+        }
+    }
+}
---
+++ arkanoid.sql
@@ -0,0 +1,14 @@
+drop table if exists usuario cascade;
+create table usuario(
+	id_u serial not null,
+	nombre varchar(100) not null,
+	constraint pk_suministrador primary key(id_u));
+
+drop table if exists puntuacion cascade;
+create table puntuacion(
+	id_p	serial not null,
+	id_u int not null,
+	puntaje int not null,
+	constraint pk_pieza primary key (id_p),
+	constraint fk_puntuacion_usuario foreign key (id_u)
+	references usuario(id_u) on delete cascade on update cascade);
